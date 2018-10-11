c2209dfac376008db882860a3141696272653779
RobinFuchshofer <robinfuchshofer@gmail.com>
2018-10-11 11:31:10 +0200

    Not finnished yet

--- assignment/pyramid_of_numbers.c
+++ assignment/pyramid_of_numbers.c
@@ -3,7 +3,7 @@
  * ---------------------------------------------------------
  * Exercise Number: 0
  * Title:			Pyramid of Numbers
- * Author:			<your name>
+ * Author:			<Robin Fuchshofer>
  * ----------------------------------------------------------
  * Description:
  * Calculates a pyramid of numbers, i.e., it multiplies a big
@@ -14,7 +14,7 @@
  */
 #include <stdio.h>
 #include <string.h>
-
+#include <stdbool.h>
 /// The maximum number of digits allowed in a big int.
 #define MAX_DIGITS 80
 
@@ -37,9 +37,22 @@ struct BigInt {
 *** @param *big_int The converted string now as BigInt.
 * @return The number of characters converted.
 */
-int strtobig_int(const char *str, int len, struct BigInt *big_int);
+int strtobig_int(const char *str, int len, struct BigInt *big_int)
 {
-
+	int converted =0;
+	int count = len;
+	len -= 1;
+	for (size_t i = 0; i < count; i++) {
+		if (str[len] < '0' || str[len] > '9')
+		{
+			return 0;
+		}
+		big_int->the_int[i] = str[len] - '0';
+		converted++;
+		len--;
+	}
+	big_int->digits_count = converted;
+	return converted;
 }
 /** print_big_int() prints a BigInt.
 *** @param *big_int The BigInt to be printed.
@@ -51,13 +64,31 @@ void print_big_int(const struct BigInt *big_int);
 *** @param factor The int value which is multiplied by BigInt.
 *** @param *big_result The result of the multiplication.
 */
-void multiply(const struct BigInt *big_int, int factor, struct BigInt *big_result);
-
-/** divide() multiplies a BigInt by an int.
-*** @param big_int The BigInt to be divided.
-*** @param divisor The int value by which we want to devide big_int.
-*** @param *big_result The result of the division.
-*/
+void multiply(const struct BigInt *big_int, int factor, struct BigInt *big_result)
+{
+	int overflowNumber=0;
+	int tempResult = 0;
+	int counter = big_int->digits_count;
+	big_result->digits_count = big_int->digits_count;
+	for (int i = 0; i < counter; i++) {
+		tempResult=big_int->the_int[i]*factor+overflowNumber;
+		overflowNumber = 0;
+		if (tempResult>9) {
+			if (i == big_int->digits_count-1) // last digit > 9
+			{
+				big_result->digits_count++;
+				big_result->the_int[i]=tempResult%10;
+				big_result->the_int[i+1]=tempResult/10;
+			} else {
+				overflowNumber=tempResult/10;
+				big_result->the_int[i]=tempResult%10;
+			}
+		}
+		else
+		{
+			big_result->the_int[i]=tempResult;
+		}
+}
 void divide(const struct BigInt *big_int, int divisor, struct BigInt *big_result);
 
 /** copy_big_int() copies a BigInt to another BigInt.
@@ -80,13 +111,40 @@ void copy_big_int(const struct BigInt *from, struct BigInt *to);
 int main(int argc, char *argv[])
 {
 	char user_input[MAX_DIGITS];
+	int length;
+	struct BigInt bigint;
+	struct BigInt bigint_result;
+	int convert;
+	int factor = 2;
 
 	printf("Pyramid of Numbers\n");
 	printf("==================\n");
 	printf("Please enter a number: ");
 	scanf("%s",user_input);
+	length = strlen(user_input);
+	convert = strtobig_int(user_input, length, &bigint);
 
-	strtobig_int(user_input, strlen(user_input));
+	if(convert == 0)
+	{
+		return 0;
+	}
+
+	printf("Number of characters converted %d \n", convert);
+
+	//multiply converted number
+	for (size_t i = 0; i < 8; i++) {
+			multiply(&bigint,factor,&bigint_result);
+			bigintarray_reverse(&bigint_result);
+			bigintarray_reverse(&bigint);
+			print_big_int(&bigint);
+			printf(" * %d = ",factor);
+			print_big_int(&bigint_result);
+			printf("\n");
+			bigintarray_reverse(&bigint_result);
+			bigintarray_reverse(&bigint);
+			bigint = bigint_result;
+			factor++;
+	}
 
 
 	return 0;
